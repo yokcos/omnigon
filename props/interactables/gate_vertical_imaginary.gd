4465c1e8cba091bfc39264781a9ca9e74c9220e3
Yokcos <yokcube@gmail.com>
2023-11-09 05:48:43 +0000

    the Ultimate Foe can no longer be removed from its room

--- props/interactables/gate_vertical_imaginary.gd
+++ props/interactables/gate_vertical_imaginary.gd
@@ -5,7 +5,12 @@ var obj_gate_fanciful: PackedScene = load("res://props/interactables/gate_vertic
 
 
 func get_shifted():
-	Game.replace_instance(self, obj_gate_fanciful.instance())
+	var index = get_position_in_parent()
+	var new_gate = obj_gate_fanciful.instance()
+	new_gate.position = global_position
+	Game.deploy_instance(new_gate, global_position)
+	
+	queue_free()
 
 func _on_entity_detector_activated() -> void:
 	$animator.play("pulsate")
